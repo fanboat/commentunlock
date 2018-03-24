ab691aa605d65c13d440ac4e772b99d4f0b6dfdd
fanboat <fanboat@gmail.com>
2018-03-23 23:06:06 -0400

    Heavy update
    Improved significantly
    Can now automatically construct database
    Updates posts first seen time by monitoring the front page, so it has data on the post from before it gets locked. Additional method monitors the post after it is locked in the event that it is unlocked before leaving the front page.
    new table 'blocked' can be used to suppress the bot from posting a given post to the subreddit even when it qualifies, allowing safe deletion of the surrogate post.

--- cubot.py
+++ cubot.py
@@ -1,5 +1,6 @@
 #commentunlockbot
 #A reddit bot by fanboat
+#reddit.com/r/commentunlock
 #fanboat.co, fanboat@gmail.com
 #2018-03-21
 
@@ -9,22 +10,26 @@ import time
 import datetime
 import MySQLdb
 import sys
-#import logging
 
 def main():
-	#logging.basicConfig(filename='/var/log/commentunlockbot.log', level=logging.INFO, format='%(asctime)s %(message)s')
 	r = bot_login()
+	#installdb() #Uncomment to install database on first run
 	open_database()
-
-	#sys.exit(0) #for test executions
+	#main loop
 	while True:
-		print("Loop start.")
+		print("Loop start")
 		lock_scan(r)
-		print("Loop complete.")
-		time.sleep(30)
+		time.sleep(1)
+		fp_scan(r)
+		print("Front page scanned, database updated")
+		fp_trim()
+		print("Old posts removed from database")
+		#update lastseen on posts which have been locked but are no longer
+		unlockedupkeep()
+		print("Loop complete")
+		time.sleep(15)
 
-
-def bot_login():
+def bot_login(): #interface with reddit's bot API
 	r = praw.Reddit(username = config.username,
 		password = config.password,
 		client_id=config.client_id,
@@ -32,75 +37,89 @@ def bot_login():
 		user_agent = 'CommentUnlockbot')
 	return r
 
-def open_database():
+def open_database(): #connect to db to log data on posts
 	global db
 	db = MySQLdb.connect('localhost', config.dbUsername, config.dbPassword, 'commentunlock')
-	#logging.info('Database connection established.')
+
+def installdb(): #Only needs to be run on initial execution. Any alterations to the db will need to be made either here before the first run, or in the db itself.
 	cursor = db.cursor()
 	query = """CREATE TABLE IF NOT EXISTS post (
-						id VARCHAR(10) PRIMARY KEY,
-						cuid VARCHAR(10),
-						title VARCHAR(300),
-						subreddit VARCHAR(20),
-						user VARCHAR(20),
-						firstseen DATETIME,
-						lastseen DATETIME)"""
+			id VARCHAR(10) PRIMARY KEY,
+			cuid VARCHAR(10),
+			title VARCHAR(300),
+			subreddit VARCHAR(20),
+			user VARCHAR(20),
+			firstlocked DATETIME,
+			lastlocked DATETIME,
+			created DATETIME,
+			firstseen DATETIME,
+			lastseen DATETIME);
+
+		CREATE TABLE IF NOT EXISTS frontpage (
+			id VARCHAR(10) PRIMARY KEY,
+			firstseen DATETIME,
+			locked TINYINT,
+			current TINYINT DEFAULT 0);
+
+		CREATE TABLE IF NOT EXISTS blocked (
+			id VARCHAR(10) PRIMARY KEY,
+			comment VARCHAR(255));"""
 	try:
 		cursor.execute(query)
 		db.commit()
 	except MySQLdb.Error as e:
 		db.rollback()
-		#logging.info("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
 	cursor.close()
 
-
-def insertnew(shortlink, cushortlink, longtitle, subreddit, user):
+def insertnew(shortlink, cushortlink, longtitle, subreddit, user, created):
 	id = shortlink[16:]
-	cuid = cushortlink[16:]
+	if cushortlink is not None:
+		cuid = cushortlink[16:]
+	else:
+		cuid = None
 	title = longtitle.encode("ascii", "ignore")
 	cursor = db.cursor()
 	now = datetime.datetime.now()
 	try:
-		cursor.execute("INSERT INTO post SELECT %s, %s, %s, %s, %s, %s, %s", (id,cuid,title,subreddit,user,str(now),str(now)))
+		cursor.execute("SELECT firstseen from frontpage WHERE id = %s""", ([id]))
+		firstseen = cursor.fetchone()
+		cursor.execute("INSERT INTO post SELECT %s, %s, %s, %s, %s, %s, %s, %s, %s, %s", (id,cuid,title,subreddit,user,str(now),str(now),created,firstseen,str(now)))
+		cursor.execute("UPDATE frontpage SET locked = 1 where id = %s", ([id]))
 		db.commit()
 	except MySQLdb.Error as e:
 		db.rollback()
-		#log?
 	cursor.close()
 
-
-def updatelastseen(shortlink):
+def updatelastlocked(shortlink, title, subreddit, user, created):
 	id = shortlink[16:]
 	cursor = db.cursor()
-
 	try:
-		#make sure it exists
 		cursor.execute("SELECT id, COUNT(*) FROM post WHERE id = %s GROUP BY id", ([id]))
 		row_count = cursor.rowcount
 		if row_count > 0:
-			#update
 			now = datetime.datetime.now()
-			cursor.execute("UPDATE post SET lastseen = %s WHERE id = %s", (str(now),id))
+			cursor.execute("UPDATE post SET lastlocked = %s WHERE id = %s", (str(now),id))
 			db.commit()
-			#print("lastseen updated")
+			print("Updated database for post " + id)
+		else:
+			insertnew(shortlink, None, title, subreddit, user, created)
 	except MySQLdb.Error as e:
 		db.rollback()
-		#log?
-
+	cursor.close()
 
 def lock_scan(r):
 	frontpage = r.subreddit('all').hot(limit=100)
-	time.sleep(2)
+	time.sleep(1)
 	try:
 		for post in frontpage:
 			if post.locked and str(post.subreddit) != "legaladvice":
 				#check if it is on the subreddit, post if not
-				crosstitle = "[" + str(post.subreddit) + "] " + post.title[:270] #the correctly formatted crosspost title
-				if len(str(post.title)) > 270:
-					crosstitle = crosstitle + "..."
-				print("Locked Post Found: "+crosstitle)
+				crosstitle = "[" + str(post.subreddit) + "] " + post.title
+				if len(str(crosstitle)) > 300:
+					crosstitle = crosstitle[:297] + "..."
+				print("Locked Post Found: " + post.shortlink[16:] + " - " + post.title[:60])
 				count = 0
-				maxcount = 15 #how many recent posts to check on commentunlock sub
+				maxcount = 20 #how many recent posts to check on commentunlock sub
 				commentunlocknew = r.subreddit('commentunlock').new(limit=maxcount)
 				for crosspost in commentunlocknew:
 					if str(crosspost.title) == crosstitle:
@@ -110,19 +129,106 @@ def lock_scan(r):
 				if count == maxcount:
 					print("No current crosspost in top "+str(maxcount)+" posts of r/commentunlock")
 					crossURL = "https://np.reddit.com" + str(post.permalink)
-					cupost = r.subreddit('commentunlock').submit(crosstitle,url=crossURL)
+					if checkblocked(post.shortlink[16:]):
+						cupost = r.subreddit('commentunlock').submit(crosstitle,url=crossURL)
+						cushortlink = cupost.shortlink
+					else:
+						cushortlink = None
 					#db entry
-					insertnew(post.shortlink,cupost.shortlink,post.title,post.subreddit,post.comments[0].author.name)
+						insertnew(post.shortlink, cushortlink, post.title, post.subreddit, post.author.name, datetime.datetime.fromtimestamp(post.created))
 					time.sleep(5)
 				else:
 					print("Crosspost already exists at position: "+str(count))
 					#db update
-					updatelastseen(post.shortlink)
-				time.sleep(2)
+					updatelastlocked(post.shortlink, post.title, post.subreddit, post.author.name, datetime.datetime.fromtimestamp(post.created))
+				time.sleep(1)
+			elif str(post.subreddit) == "commentunlock":
+				#We're on the front page??
+				print("Front Page!?")
+				#modcomment = post.reply("We seem to be on the front page. I do not believe /u/fanboat is prepared for this.")
+				#modcomment.distinguish("yes",True)
 	except HttpError:
 		print("HTTP error?")
-		#logging.warning("HTTP Error caught. Ignoring...")
 
+def fp_scan(r):
+	frontpage = r.subreddit('all').hot(limit=100)
+	time.sleep(1)
+	try:
+		for post in frontpage:
+			#insert any which are not in db
+			id = str(post.shortlink)[16:]
+			if post.locked:
+				locked = 1
+			else:
+				locked = 0
+			fp_insert(id,locked)
+	except HttpError:
+		print("HTTP error2?")
+
+def fp_insert(id, locked):
+	cursor = db.cursor()
+	now = datetime.datetime.now()
+	try:
+		cursor.execute("""INSERT INTO frontpage
+			SELECT %s, %s, %s, 1
+			ON DUPLICATE KEY UPDATE current = 1, locked = %s""", (id, now, locked, locked))
+		db.commit()
+	except MySQLdb.Error as e:
+                db.rollback()
+	cursor.close()
+
+def fp_trim():
+	fp_trimA()
+	fp_trimB()
+
+def fp_trimA():
+	cursor = db.cursor()
+	try:
+		cursor.execute("DELETE FROM frontpage WHERE current = 0")
+		db.commit()
+	except MySQLdb.Error as e:
+		db.rollback()
+	cursor.close()
+
+def fp_trimB():
+	cursor = db.cursor()
+	try:
+		cursor.execute("UPDATE frontpage SET current = 0")
+		db.commit()
+	except MySQLdb.Error as e:
+		db.rollback()
+	cursor.close()
+
+def checkblocked(id):
+	cursor = db.cursor()
+	try:
+		cursor.execute("SELECT id from blocked WHERE id = %s", ([id]))
+		blockcheck = cursor.fetchone()
+		if blockcheck is None:
+			print("Post is allowed")
+			return True
+		else:
+			print("Post is in blocked list")
+			return False
+	except MySQLdb.Error as e:
+		db.rollback()
+		return False
+	cursor.close()
+
+def unlockedupkeep():
+	#This function will check up on posts which were once locked on the front page, but were unlocked before they fell off
+	cursor = db.cursor()
+	now = datetime.datetime.now()
+	try:
+		cursor.execute("""UPDATE post p
+				inner join frontpage f on
+					f.id = p.id
+				set p.lastseen = %s;""", [str(now)])
+		db.commit()
+		print("Lastseen updated")
+	except MySQLdb.Error as e:
+		db.rollback()
+	cursor.close()
 
 if __name__ == '__main__':
 	main()
